fd587dc2f6189d0668c2f4c1202f3f4509239274
tillarnold <throwable42@gmail.com>
2014-05-10 19:41:13 +0200

    Added test boilerplate for domHelpers

---
+++ test/browser/domHelpersTest.js
@@ -0,0 +1,18 @@
+//Defined by mocha
+/* global describe: false */
+/* global it: false */
+
+//Should tests are allowed
+/* jshint -W030 */
+
+//var domHelpers = require('../lib/domHelpers');
+require('should');
+
+describe('domHelpers', function() {
+  describe('#arrayToFragment()', function() {
+    it('should turrn an array into a fragment', function() {});
+  });
+  describe('#nodeListToArray()', function() {});
+  describe('#elementToFragment()', function() {});
+  describe('#nodeListToFragment()', function() {});
+});
